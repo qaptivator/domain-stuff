e4f86a27be406716e9d028c308e2f320928c0f28
captivator <84224501+qaptivator@users.noreply.github.com>
2025-03-10 18:59:22 +0200

    changes

---
+++ components/NumberInput.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="flex gap-4">
+		<span class="text-2xl"><slot /></span>
+		<input
+			class="rounded-md p-1 border bg-slate-200 w-32"
+			type="number"
+			v-model="model"
+			placeholder="0"
+		/>
+	</div>
+</template>
+<script setup lang="ts">
+const model = defineModel()
+</script>
+<style scoped lang="css"></style>
--- pages/math/quadratic.vue
+++ pages/math/quadratic.vue
@@ -1,11 +1,56 @@
 <template>
 	<div class="p-4 text-xl">
 		<h1>> quadratic formula solver</h1>
-		<p class="mb-8">a collection of tools to check your mouse's performance</p>
+		<p class="mb-8">solve that pesky quadratic formula</p>
+		<Math
+			v-model="formula"
+			class="mb-8"
+		/>
+		<div class="flex flex-col gap-2 mb-8">
+			<NumberInput v-model="a">a: </NumberInput>
+			<NumberInput v-model="b">b: </NumberInput>
+			<NumberInput v-model="c">c: </NumberInput>
+		</div>
 		<Math v-model="expr" />
 	</div>
 </template>
 <script setup lang="ts">
-const expr = ref('c = \\pm\\sqrt{a^2 + b^2}')
+const formula = ref('x = \\frac{-b \\pm \\sqrt{b^2 - 4ac}}{2a}, a \\neq 0')
+const expr = ref('')
+const a = ref(null)
+const b = ref(null)
+const c = ref(null)
+
+function updateExpr() {
+	if (a.value === null || b.value === null || c.value === null) {
+		expr.value = ''
+		return
+	}
+	const d = b.value ** 2 - 4 * a.value * c.value
+	let x1 = NaN
+	let x2 = NaN
+
+	if (d < 0 || a.value === 0) {
+		//x1 = NaN
+		expr.value = `\\text{No real roots}`
+		return
+	} else if (d === 0) {
+		x1 = -b.value / (2 * a.value)
+		//expr.value = `x = \\frac{-b}{2a}`
+		//expr.value = `x = \\frac{-${b.value}}{2 \\cdot ${a.value}} = ${x1}`
+		//const bstr = Math.sign(a.value) === 1 ? a.value : `(${a.value})`
+		expr.value = `x = \\frac{-${b.value} \\pm \\sqrt{${b.value}^2 - 4 \\cdot ${a.value} \\cdot ${c.value}}}{2 \\cdot ${a.value}}`
+		return
+	} else if (d > 0) {
+		x1 = (-b.value + Math.sqrt(d)) / (2 * a.value)
+		x2 = (-b.value - Math.sqrt(d)) / (2 * a.value)
+		//expr.value = `x = \\frac{-b \\pm \\sqrt{b^2 - 4ac}}{2a}`
+		return
+	}
+}
+
+watch(a, updateExpr)
+watch(b, updateExpr)
+watch(c, updateExpr)
 </script>
 <style scoped lang="css"></style>
