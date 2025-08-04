fd4e618f7c924652b2b23c623a53b1bd5c348221
captivator <84224501+qaptivator@users.noreply.github.com>
2025-08-05 00:01:27 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,18 +6,23 @@
 				<b>domain stuff</b>
 			</p>
 			<p>
-				> you can use a bunch of useful utility tools, without all the ads and
-				popups
+				> you can use a bunch of useful utility tools, mostly for programming,
+				without all the ads and popups
 			</p>
 			<p>> it's nothing special, really, mostly just for my personal use</p>
 			<p>
-				> feel free to look around, and maybe even star the
+				> i often integrate very useful tools into this website when im too lazy
+				to google for them every time,<br />and im letting other people use them
+				too
+			</p>
+			<p>
+				> feel free to look around, and maybe even give a star to the
 				<NuxtLink
 					to="https://github.com/qaptivator/domain-stuff"
 					target="_blank"
 					>github</NuxtLink
 				>
-				repository...
+				repository... (pretty please)
 			</p>
 			<p>> created by qaptivator</p>
 		</div>
